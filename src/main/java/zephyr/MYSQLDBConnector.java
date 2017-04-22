c5a74d6b45d3c5e59f11343f53667e23bf473815
MiOnim <monim1@pride.hofstra.edu>
2017-04-22 18:19:34 -0400

    updated dbconnector class

--- src/main/java/zephyr/MYSQLDBConnector.java
+++ src/main/java/zephyr/MYSQLDBConnector.java
@@ -1,32 +1,57 @@
-package zephyr;
+package main.java.zephyr;
 
 import java.sql.*;
 
-public class MYSQLDBConnector {
-	
+public class MYSQLDBConnector
+{
 	private static final String CLASSNAME = "com.mysql.jdbc.Driver"; 
-	private static final String RDS_HOSTNAME = "jdbc:mysql://zepyhrdb.cb2tfamnh1z2.us-east-1.rds.amazonaws.com:3306/user";
+	private static final String RDS_HOSTNAME = "jdbc:mysql://zepyhrdb.cb2tfamnh1z2.us-east-1.rds.amazonaws.com:3306/";
 	private static final String RDS_PASSWORD = "hackru2017";
-	private static final String RDS_DATABASE = "user";
 	private static final String RDS_USERNAME = "admin";
-	private static final String query = "SELECT * from employees";
 	
-	public static void initialize() {
-		try{  
-				
-				Class.forName(CLASSNAME);  
-				Connection con = DriverManager.getConnection(RDS_HOSTNAME,
-														RDS_USERNAME,
-														RDS_PASSWORD);
-				
-				java.sql.Statement stmt=con.createStatement();  
-				ResultSet rs = stmt.executeQuery(query);  
-				while(rs.next())  
-				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-				con.close();  
-			}catch(Exception e){ 
-				System.out.println(e);
-			}  
-	}  
-
+	public String dbName = "";
+	public String dbUrl = "";
+	protected boolean connected = false;
+	
+	protected Connection conn;
+	
+	public MYSQLDBConnector(String dbName)
+	{
+	    this.dbName = dbName;
+	    this.dbUrl = RDS_HOSTNAME + dbName;
+	}
+	
+	public boolean initialize()
+	{
+	    boolean retval = true;
+	    
+	    try {
+	        Class.forName(CLASSNAME);
+	
+	        conn = DriverManager.getConnection(dbUrl,RDS_USERNAME,RDS_PASSWORD);
+	        
+	        connected = true;
+	    } catch(Exception e) {
+	    	retval = false;
+	    } 
+	    
+	    return retval;
+	}
+	
+	public ResultSet executeQuery(String query) throws Exception
+    {
+        if (!connected) {
+            initialize();
+        }
+        
+        ResultSet results = null;
+        
+        try {
+            Statement stmt = conn.createStatement();
+            results = stmt.executeQuery(query);
+        } catch (SQLException e) {
+            throw new Exception("executeQuery(): error");
+        }
+        return results;
+    }
 }
